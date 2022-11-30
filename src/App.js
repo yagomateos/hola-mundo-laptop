9bc31ddb522702d4736827a38bb79ffac19cbfe1
Yago <yagomateos@hotmail.com>
2022-11-30 03:40:57 +0100

    changelog

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './components/pure/greeting';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/*Componente propio greeting.jsx*/}
+          <Greeting name="Martín"></Greeting>
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         
         </a>
       </header>
     </div>
